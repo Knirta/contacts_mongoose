12c22d52346078872f62bd560ba2522077a73fd6
Knirta <myknirtak@gmail.com>
2025-08-06 15:51:15 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+import mongoose from "mongoose";
+
+const DB_HOST =
+  "mongodb+srv://Knirta:Knirta1984@cluster0.g35o2vw.mongodb.net/contacts_db?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
